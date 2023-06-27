f074eec02b256eadf4a758e7d9c7c1a189d1fd66
Lucifer-Aspire-A315-21 <omkaromu183@gmail.com>
2023-06-27 13:14:40 +0530

    day-1

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ideal_skills/responsive/mobile_screen_layout.dart';
+import 'package:ideal_skills/responsive/responsive_layout.dart';
+import 'package:ideal_skills/responsive/web_screen_layout.dart';
+import 'package:ideal_skills/utils/colors.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark()
+          .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
+      title: 'ideal_skills ',
+      home: const ResponsiveLayout(
+        webScreenLayout: WebScreenLayout(),
+        mobileScreenLayout: MobileScreenLayout(),
+      ),
+    );
+  }
+}
